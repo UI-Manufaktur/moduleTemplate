a14a57334dce49e30536c34ef8d56208c8633b35
UIM (powered by Sicherheitsschmiede) <ui.manufaktur@gmail.com>
2022-07-05 06:31:53 +0200

    Update package.d

--- source/models/myName/attributes/package.d
+++ source/models/myName/attributes/package.d
@@ -4,4 +4,5 @@ public import models.myName.attributes.codes;
 public import models.myName.attributes.elements;
 public import models.myName.attributes.entites;
 public import models.myName.attributes.functions;
+public import models.myName.attributes.relationships;
 public import models.myName.attributes.uuids;
